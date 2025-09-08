6fc4eebf5fe7214aaa75292a44e651b1c316670d
Ikaro <nunesikaro@gmail.com>
2025-09-08 13:06:39 -0300

    if e else

---
+++ if e else/logica.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int maça = 50;
+    int banana = 45;
+
+    if(maça==banana)    //numero de maças e igual a de bananas
+        printf("verdadeiro");   //se for exibe verdadeiro
+    else    
+        printf("e falso");  //se nao e falso
+
+}
