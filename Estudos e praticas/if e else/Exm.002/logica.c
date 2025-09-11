5fd1342ed6d929b6251c30ecdfe3b4af361b3cfa
Your Name <nunesikaro@gmail.com>
2025-09-11 04:08:40 -0300

    add exemplo 002 if e else

---
+++ Estudos e praticas/if e else/Exm.002/logica.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 16;
+
+    if(idade >= 16)
+        printf("Pode votar!");
+    else
+        printf("não pode votar!");
+
+    return 0;
+}
+
+//vimos que o "if" mostra que se a idade for maior ou igual a 16, vai exibir que pode votar, o "else" que é "Se não" vai exibir que não pode votar!
