f0e114d85e5a404de002c0ddee67073d05fe0939
Your Name <nunesikaro@gmail.com>
2025-09-15 01:41:35 -0300

    reajuste completo desafio basico tema2

--- supertrufo Basico/tema2/SuperTrunfoDesafio/SupertrunfoFinal.c
+++ supertrufo Basico/tema2/SuperTrunfoDesafio/SupertrunfoFinal.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    char estado1 = 'A', estado2 = 'B';
-    char cidade1[40] = "CidadeA", cidade2[40] = "CidadeB";
-    char codigo1[3] = "A01", codigo2[3] = "A02";
-    int populacao1 = 100000, populacao2 = 200000;
-    float area1 = 1500.5, area2 = 2500.75;
-    float pib1 = 50000.0, pib2 = 75000.0;
-    int pontosturisticos = '1', pontosturisticos2 = '2';
+    char estado1, estado2;
+    char cidade1[40], cidade2[40];
+    char codigo1[5], codigo2[5];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontosturisticos, pontosturisticos2;
 
-    //Entrada e Saida de dados da carta 1
+    //Entrada e Saida de dados da carta 1 e carta 2
     
-              printf("\n=== Carta 1 ===\n"); 
+            printf("\n=== Carta 1 ===\n"); 
               
     printf("qual seu Estado?:");
     scanf("%c", &estado1);
     printf("qual sua Cidade?:");
     scanf("%s", &cidade1);
     printf("qual seu Codigo?:");
-    scanf("%s", &codigo1);
+    scanf("%s", codigo1);
     printf("qual sua População?:");
     scanf("%d", &populacao1);
     printf("qual sua Área?:");
@@ -28,22 +28,7 @@ int main() {
     printf("qual seus Pontos Turísticos?:");
     scanf("%d", &pontosturisticos);
 
-     printf("\n=== Carta 1 ===\n"); 
-
-     //Exibição dos dados da carta 1 no terminal 
-    
-     printf("Estado: %c\n", estado1);
-    printf("Cidade: %s\n", cidade1);
-    printf("Código: %s\n", codigo1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.2f\n", area1);
-    printf("PIB: %.2f\n", pib1);
-    printf("Pontos Turísticos: %d\n", pontosturisticos);
-
-        //Carta 1 finalizada
-    
-         printf("\n=== Carta 2 ===\n");
-    //Entrada e Saida de dados da carta 2
+           printf("\n=== Carta 2 ===\n");
 
     printf("Qual seu Estado?:");
     scanf(" %c", &estado2);
@@ -60,18 +45,38 @@ int main() {
     printf("Qual seus Pontos Turísticos?: ");
     scanf("%d", &pontosturisticos2);
 
+    
+
+     //Exibição dos dados da carta 1 e carta 2 no terminal 
+    
+        printf("\n=== Carta 1 ===\n"); 
+
+     printf("Estado: %c\n", estado1);
+    printf("Cidade: %s\n", cidade1);
+    printf("Código: %s\n", codigo1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Pontos Turísticos: %d\n", pontosturisticos);
+
+        //Carta 1 finalizada
+    
+  
     printf("\n=== Carta 2 ===\n"); 
     
     printf("Estado: %c\n", estado2);
     printf("Cidade: %s\n", cidade2);
     printf("Código: %s\n", codigo2);
     printf("População: %d\n", populacao2);
-    printf("Área: %.2f\n", area2);
-    printf("PIB: %.2f\n", pib2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Pontos Turísticos: %d\n", pontosturisticos2);
 
+        //carta 2 finalizada
+
      //Exibição dos dados da carta 2 no terminal
-    return 0;
+   
+     return 0;
 
 
 }
