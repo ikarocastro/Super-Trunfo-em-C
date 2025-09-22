e5affdbb0c548d60eca31b2e4339aa03d966adf2
Your Name <nunesikaro@gmail.com>
2025-09-21 23:48:18 -0300

    criei jogo jo ken po, hr de codar

--- Estudos e praticas/Tema 3/if e else/Nivel aventureiro/hora de codar/jokenpo.c
+++ Estudos e praticas/Tema 3/if e else/Nivel aventureiro/hora de codar/jokenpo.c
@@ -12,13 +12,61 @@ int main() {
     printf("1. pedra\n");
     printf("2. papel\n");
     printf("3. tesoura\n");
-    printf("Escolha: \n");
+    printf("Escolha: ");
     scanf("%d", &escolhaJogador);
 
     escolhaComputador = rand () % 3 + 1;
     
+    switch (escolhaJogador)
+    {
+    case 1:
+        printf("Jogador: pedra - ");
+        break;
+    case 2:
+        printf("Jogador: Papel - ");
+        break;
+    case 3:
+    printf("Jogador: Tesoura - ");
+        break;
+    default:
+        printf("Opção invalida!");
+        break;
+    }
 
+    switch (escolhaComputador)
+    {
+     case 1:
+        printf("Computador: pedra\n ");
+        break;
+    case 2:
+        printf("Computador: Papel\n ");
+        break;
+    case 3:
+    printf("Computador: Tesoura\n ");
+        break;
+    default:
+        printf("Opção invalida!\n"); 
+        break;
+    }
+ 
+        if (escolhaJogador == escolhaComputador)
+        {
+            printf("### Empate ###\n");
 
+        }else if ((escolhaJogador == 1) && (escolhaComputador == 3 ) ||
+                    (escolhaJogador == 2) && (escolhaComputador == 1) ||
+                    (escolhaJogador == 3) && (escolhaComputador == 2))
+        {
+            
+            printf("*** Parabéns, Vitória do Jogador! ***\n");
+            
+         }else{
+                printf("*** Infelizmente, Você perdeu! ***\n");
+        
+        }
+        
+        
+        
     return 0;
 
 }
